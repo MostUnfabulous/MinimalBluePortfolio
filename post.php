62214d966396a3cfa4be5027dc6987291730f1fe
Hayden Smith <Haydenbsmith@gmail.com>
2017-05-25 20:08:04 +0100

    form fix Fname

--- post.php
+++ post.php
@@ -1,5 +1,7 @@
 <? php
-$name = $_POST['Fname' 'Sname'];
+
+if( isset($_POST) ){
+$name = $_POST['Fname'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
@@ -8,4 +10,6 @@ $to = 'haydenbsmith@gmail.com';
 $subject = 'Message from Contact Demo ';
 
 $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+}
 ?>
